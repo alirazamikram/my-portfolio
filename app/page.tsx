15ccae33ed26846b69dcb81917edb4b16d78d510
ALIRAZA <alirazamikraam@gmail.com>
2023-12-29 23:11:16 +0500

    feat: move component folder outside from app

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
+import HeaderSection from "@/components/common/Sections/HeaderSection";
+import ProjectSection from "@/components/common/Sections/ProjectSection";
+import Navbar from "@/components/layout/Navbar";
 import React from "react";
-import Navbar from "./components/layout/Navbar";
-import HeaderSection from "./components/common/Sections/HeaderSection";
-import ProjectSection from "./components/common/Sections/ProjectSection";
 export default function Home() {
   return (
     <div className="bg-primary font-poppins ">
